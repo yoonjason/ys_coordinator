83fcba8a8a0c2130d6bb22342baa604e6ca9bbf5
Bradley.yoon <bradley.yoon@kakaocorp.com>
2023-04-03 11:57:51 +0900

    update: 스토리보드내 뷰컨트롤러 불러오는 로직 추가

--- Sources/ys_coordinator/ys_coordinator.swift
+++ Sources/ys_coordinator/ys_coordinator.swift
@@ -1,3 +1,5 @@
+import UIKit
+
 public protocol Coordinator: AnyObject {
     func start()
     func coordinate(to coordinator: Coordinator)
@@ -7,4 +9,9 @@ extension Coordinator {
     func coordinate(to coordinator: Coordinator) {
         coordinator.start()
     }
+}
+
+public func new(name: String, storyboard: String = "") -> UIViewController {
+    let storybordName = !storyboard.isEmpty ? storyboard : name
+    return UIStoryboard(name: "\(storybordName)", bundle: nil).instantiateViewController(withIdentifier: name)
 }
